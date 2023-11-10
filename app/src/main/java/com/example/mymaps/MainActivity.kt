6adac0282259c2346f9b6bcdceccd8c1a7060584
Jhon smith <jhonsenapro@gmail.com>
2023-11-10 15:36:27 -0500

    nuevos elements agregados al zoom

--- app/src/main/java/com/example/mymaps/MainActivity.kt
+++ app/src/main/java/com/example/mymaps/MainActivity.kt
@@ -2,9 +2,12 @@ package com.example.mymaps
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(),OnMapReadyCallback {
     private lateinit var map:GoogleMap
@@ -12,6 +15,7 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        createFragment()
     }
     private fun createFragment() {
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
@@ -20,5 +24,15 @@ class MainActivity : AppCompatActivity(),OnMapReadyCallback {
 
     override fun onMapReady(googleMap: GoogleMap) {
         map = googleMap
+        createMarker()
+    }
+    private fun createMarker(){
+        val coordinates =LatLng(2.432908, -76.608853)
+        val marker : MarkerOptions = MarkerOptions().position(coordinates).title("mi casa")
+        map.addMarker(marker)
+        map.animateCamera(
+            CameraUpdateFactory.newLatLngZoom(coordinates,15f),4000,null
+        )
+
     }
 }
